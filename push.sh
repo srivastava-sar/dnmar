1c59b0a7cd6ecce6d0d30c6213a0d17676c69ada
Alan <aritter@aritter-UX31E.(none)>
2012-06-19 15:37:41 -0700

    fixed push

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-git push ssh://rv-n11.cs.washington.edu/home/aritter/calendar_demo/twitter_calendar_v3 master:refs/remotes/milkhog.cs.washington.edu/master
+git push ssh://rv-n11.cs.washington.edu/home/aritter/dlvm master:refs/remotes/milkhog.cs.washington.edu/master
