4d3013d0883fc9f5ba3115d258a5bfb345a84a0e
Alan <aritter@aritter-UX31E.(none)>
2012-11-19 00:05:37 -0800

    generates pdf

--- scripts/plotPR.R
+++ scripts/plotPR.R
@@ -1,11 +1,17 @@
+#png('PR.png', width=1500, height=1000)
+pdf('PR.pdf', width=20, height=15)
 files = list.files('../scala/experiments/dnmar_random20')
-par(mfcol=c(4,6))
+par(mfcol=c(3,4))
 for(f in files) {
   if(f != 'log') {
     print(f)
     p1 = read.csv(paste('../scala/experiments/dnmar_random20/',f,sep=""), sep="\t", header=FALSE)
     p2 = read.csv(paste('../scala/experiments/multir/',f,sep=""), sep="\t", header=FALSE)
-    plot(p1[,2], p1[,1], xlim=c(0,0.9), ylim=c(0,1), main=f, xlab='recall', ylab='precision')
-    points(p2[,2], p2[,1], col='red')
+
+    if(sum(p1[,1] > 0) && sum(p2[,1] > 0)) {
+      plot(p1[,2], p1[,1], xlim=c(0,0.9), ylim=c(0,1), main=f, xlab='recall', ylab='precision')
+      points(p2[,2], p2[,1], col='red')
+    }
   }
 }
+dev.off()
