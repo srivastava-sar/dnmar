8dd264961ce39b77641411d1bffbb53a5ca3dc4e
Alan <aritter@aritter-UX31E.(none)>
2013-06-02 21:44:15 -0700

    added R scripts for plotting figures
    Former-commit-id: 07d3103762430a8ccffd1974b86b1a1eeaa8752f

---
+++ scripts/plotPRsentential_NER.R
@@ -0,0 +1,38 @@
+pdf('PR_sentential_NER.pdf', width=7, height=7)
+
+f = 'sentential'
+file = paste('../scala/experiments/NER_DNMAR/',f,sep="")
+if(file.exists(file) && !file.info(file)$isdir && file.info(file)$size > 0) {
+  p1 = read.csv(file, sep="\t", header=FALSE)
+} else {
+  p1 = data.frame(p=numeric(0), r=numeric(0))
+}
+                                        #p2 = read.csv(paste('../scala/experiments/multir_sent/',f,sep=""), sep="\t", header=FALSE)
+file = paste('../scala/experiments/NER_MultiR/',f,sep="")
+if(file.exists(file) && !file.info(file)$isdir && file.info(file)$size > 0) {
+  p2 = read.csv(file, sep="\t", header=FALSE)
+} else {
+  p2 = data.frame(p=numeric(0), r=numeric(0))
+}
+#file = paste('../scala/experiments/NER_DNMAR_fb/',f,sep="")
+#if(file.exists(file) && !file.info(file)$isdir && file.info(file)$size > 0) {
+#  p3 = read.csv(file, sep="\t", header=FALSE)
+#} else {
+#  p3 = data.frame(p=numeric(0), r=numeric(0))
+#}    
+
+if(dim(p1)[1] > 20 && (sum(p1[,1] > 0) || sum(p2[,1] > 0))) {
+                                        #plot(p1[,2], p1[,1], xlim=c(0,0.9), ylim=c(0,1), main=f, xlab='recall', ylab='precision')
+  if(f == 'aggregate') {
+    plot(p1[,2], p1[,1], xlim=c(0,0.3), ylim=c(0.15,1), xlab='recall', ylab='precision')
+  } else {
+    plot(p1[,2], p1[,1], xlim=c(0,1), ylim=c(0,1), xlab='recall', ylab='precision')
+  }
+  points(p2[,2], p2[,1], col='red', pch=2)
+  #points(p3[,2], p3[,1], col='green', pch=3)
+}
+
+#legend('bottomleft', c('NER_MultiR', 'NER_DNMAR', 'NER_DNMAR*'), pch=c(2,1,3), col=c('red', 'black', 'green'))
+legend('topright', c('NER_MultiR', 'NER_DNMAR'), pch=c(2,1), col=c('red', 'black'))
+
+dev.off()
